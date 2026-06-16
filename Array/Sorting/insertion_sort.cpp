7067177959cd655a9492172549d99adbeefb7ba0
Sriram ray <173797061+Sriramray567@users.noreply.github.com>
2026-06-16 10:59:10 +0530

    Implement insertion sort algorithm

--- Array/Sorting/insertion_sort.cpp
+++ Array/Sorting/insertion_sort.cpp
@@ -1,1 +1,16 @@
-
+void insertionSort(vector<int>& arr) {
+        // Loop from the second element (index 1) to the end of the array
+        int n=arr.size();
+        for(int i=1; i<n; i++){
+            int key = arr[i];
+             int j = i-1; 
+          // Shift elements of arr[0..i-1] that are greater than the key
+          //to one position ahead of their current position
+             while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+             }
+          //Place the key into its correct position
+             arr[j+1]=key;
+        }
+    }
